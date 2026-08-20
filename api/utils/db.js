bcff7bdd1b8db218ab2c3ce477efdb5a4f5282f2
Jaiakash <jaiakash@example.com>
2026-08-20 11:44:54 +0530

    fix: mongoose serverless connection reuse logic

--- api/utils/db.js
+++ api/utils/db.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 
-let cachedDb = null;
-
 async function connectToDatabase() {
-    if (cachedDb) return cachedDb;
-    
     if (!process.env.MONGODB_URI) {
         throw new Error('Please define the MONGODB_URI environment variable');
     }
 
-    const opts = { bufferCommands: false };
-    const db = await mongoose.connect(process.env.MONGODB_URI, opts);
-    cachedDb = db;
-    return db;
+    if (mongoose.connection.readyState === 1) {
+        return mongoose.connection.asPromise();
+    }
+
+    const opts = { bufferCommands: false, serverSelectionTimeoutMS: 5000 };
+    return await mongoose.connect(process.env.MONGODB_URI, opts);
 }
 
 module.exports = connectToDatabase;
